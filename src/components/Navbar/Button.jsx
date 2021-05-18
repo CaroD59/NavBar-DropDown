7ee9adbf94fe73103e0cb3d77999ec969e05f9d5
CaroD59 <dorchies.c@gmail.com>
2021-05-18 11:41:49 +0200

    NavBar Created

--- src/components/Navbar/Button.jsx
+++ src/components/Navbar/Button.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import './ButtonCSS';
+import ButtonCSS from './ButtonCSS';
 import { Link } from 'react-router-dom';
 
 export function Button() {
   return (
-    <Link to='sign-up'>
-      <button className='btn'>Sign up</button>{' '}
-    </Link>
+    <ButtonCSS>
+      <Link to='sign-up'>
+        <button className='btn'>Sign up</button>{' '}
+      </Link>
+    </ButtonCSS>
   );
 }
---
+++ src/components/Navbar/ButtonCSS.jsx
@@ -0,0 +1,4 @@
+import styled from 'styled-components';
+
+const ButtonCSS = styled.div``;
+export default ButtonCSS;
